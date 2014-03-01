3b2c8d9f46b523ff6afcd0c0c25092e2f49b5323
Med Elharrak <med.elharrak@gmail.com>
2014-03-01 14:43:53 +0000

    dry working version

--- js/controllers/cart-controllers.js
+++ js/controllers/cart-controllers.js
@@ -3,10 +3,51 @@
 
 var cartCtrl = angular.module('cartCtrl', []);
 
-cartCtrl.controller('ListCtrl', ['$scope', 'Items', function ($scope, Items) {
+// dislpay a list of items
+cartCtrl.controller('ListCtrl', ['$scope', 'Items', 'CartStorage', function ($scope, Items, CartStorage) {
+    // fetch items list and display it 
     $scope.items = Items.get();
+
+    // Invoked whenever 'add to cart' button is clicked
+    $scope.addToCart = function (item) {
+        CartStorage.addItem({
+            id: item.id,
+            quantity: item.quantity
+        });
+    };
 }]);
 
-cartCtrl.controller('CheckoutCtrl', [
-    function () {}
-]);
+// display the content of the shopping cart
+cartCtrl.controller('CheckoutCtrl', ['$scope', 'Items', 'CartStorage', function ($scope, Items, CartStorage) {
+
+    // Initialize cart items from localStorage
+    $scope.cart = CartStorage.getCart();
+    // the cart knows only item IDs and their quantity 
+    // hence, we need items list to retrieve brand and labels
+    $scope.items = Items.get();
+
+
+    $scope.changeQuantity = function (item) {
+        CartStorage.changeQuantity(item.id, item.quantity);
+    };
+
+    $scope.removeFromCart = function (item) {
+        CartStorage.remove(item.id);
+        // update the cart manually because
+        // some browser do not fire storage change on same window
+        delete $scope.cart[item.id];
+    };
+
+    $scope.clear = function () {
+        CartStorage.clear();
+        $scope.cart = []; // Update the cart manually
+    };
+
+    // update the model whenever the storage is modifieded
+    // this allow multiple browser tabs to have the same cart information
+    $scope.$on('storageUpdate', function () {
+        $scope.$apply(function () {
+            $scope.cart = CartStorage.getCart();
+        });
+    });
+}]);
--- js/services/cart-services.js
+++ js/services/cart-services.js
@@ -1,27 +1,97 @@
-/*global angular */
+/*global angular, localStorage, window */
 'use strict';
 
 var cart = angular.module('services.cart', []);
 
 // Predefined items to fill our shop
-cart.service('Items', [ function () {
-    this.get = function () {
-        return {
-            '1': {
-                id: 1,
-                brand: 'Nike',
-                label: "Lunargato II"
-            },
-            '2': {
-                id: 2,
-                brand: 'adidas',
-                label: "Nitrocharge 2.0 Trx FG"
-            },
-            '3': {
-                id: 3,
-                brand: 'Puma',
-                label: "Xenon TR SL2"
+cart.service('Items', [
+    function () {
+        this.get = function () {
+            return {
+                '1': {
+                    id: 1,
+                    brand: 'Nike',
+                    label: "Lunargato II"
+                },
+                '2': {
+                    id: 2,
+                    brand: 'adidas',
+                    label: "Nitrocharge 2.0 Trx FG"
+                },
+                '3': {
+                    id: 3,
+                    brand: 'Puma',
+                    label: "Xenon TR SL2"
+                }
+            };
+        };
+    }
+]);
+
+
+cart.service('CartStorage', ['$rootScope',
+    function ($rootScope) {
+        var STORAGE_ID = "cart";
+
+        this.getCart = function () {
+            return JSON.parse(localStorage.getItem(STORAGE_ID) || '{}');
+        };
+
+        this.addItem = function (item, noPersist) {
+            var cart = this.getCart();
+            if (cart[item.id]) {
+                cart[item.id].quantity += parseInt(item.quantity, 10);
+            } else {
+                cart[item.id] = item;
+            }
+            if (noPersist) {
+                return;
+            }
+            this.persist(cart);
+        };
+
+        this.addItems = function (items) {
+            var id, cart = this.getCart();
+
+            for (id in items) {
+                if (items.hasOwnProperty(id)) {
+                    this.addItem(items[id], true);
+                }
+            }
+            this.persist(cart);
+        };
+
+        this.save = function () {};
+
+        this.remove = function (id) {
+            var cart = this.getCart();
+            delete cart[id];
+            this.persist(cart);
+        };
+
+        this.clear = function () {
+            this.persist({});
+        };
+
+        this.persist = function (cart) {
+            localStorage.setItem(STORAGE_ID, JSON.stringify(cart));
+        };
+
+        this.changeQuantity = function (id, quantity) {
+            var cart = this.getCart();
+            if (cart[id]) {
+                cart[id].quantity = parseInt(quantity, 10);
+                this.persist(cart);
             }
         };
-    };
-}]);
+
+        this.refresh = function () {
+            $rootScope.$broadcast('storageUpdate');
+        };
+
+        if (window.addEventListener) {
+            window.addEventListener("storage", this.refresh, false);
+        } else {
+            window.attachEvent("onstorage", this.refresh);
+        }
+    }]);
